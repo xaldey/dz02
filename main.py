fe9b4b384dc6b38f5e7e6ad4fad93d75946aada7
xaldey <pencil_2004@mail.ru>
2020-03-10 00:17:03 +0300

    yandex search complete
    Надо только доделать google и начать сортировку выдачи обоих

--- main.py
+++ main.py
@@ -1,4 +1,41 @@
 import requests
 
-r = requests.get('https://yandex.ru', data={'search':'pypi'})
-print(r.text)
+def YandexSearch(query):
+	    with requests.session() as c:
+	        url = 'https://yandex.ru/search/?'
+	        query = {'text': user_search_string}
+	        urllink = requests.get(url, params=query)
+	        print (urllink.url)
+	        print (urllink.text)	
+
+def googleSearch(query):
+	    with requests.session() as c:
+	        url = 'https://www.google.com/search?'
+	        query = {'q': user_search_string}
+	        urllink = requests.get(url, params=query)
+	        print (urllink.url)
+
+
+user_engine_choice = input('Укажите поисковый движок? (yandex <> google): ')
+print(user_engine_choice)
+
+if user_engine_choice == 'yandex':
+	user_engine_string = 'https://yandex.ru'
+	print("Будем искать в", user_engine_string)
+	user_search_string = input("Что надо найти? ")
+	YandexSearch(user_search_string)
+else:
+	user_engine_string = 'https://google.com'
+	print("Будем искать в", user_engine_string)
+	user_search_string = input("Что надо найти? ")
+	googleSearch(user_search_string)
+
+#resp = requests.get(user_engine_string, data={'text':user_search_string})
+
+# if urllink:
+# 	print ('Success')
+# else:
+# 	print('Error happens')
+# 	print(urllink.status_code)
+
+#print(resp.text)
